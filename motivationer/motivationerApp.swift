7fd065485372971bafb4f583493aabb77cf5f9ae
arniekanwal <arnavkanwal@gmail.com>
2023-10-29 22:20:10 -0400

    Initialize xcode project

---
+++ motivationer/motivationerApp.swift
@@ -0,0 +1,17 @@
+//
+//  motivationerApp.swift
+//  motivationer
+//
+//  Created by Arnav Kanwal on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct motivationerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
